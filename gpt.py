443e02bf26ee300b7165d0039c42a8e6fe10aaaf
Luc von Niederhäusern <lucv@student.ethz.ch>
2023-05-15 13:01:38 +0200

    minor changes to the training parameters

--- gpt.py
+++ gpt.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 5000
+max_iters = 500
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
